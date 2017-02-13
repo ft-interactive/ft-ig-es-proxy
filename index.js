541d0058c42ba01ada37b7f581de6206470a38cb
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-02-13 18:07:37 +0000

    Should work

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 
 const express = require('express');
 const authS3O = require('@financial-times/s3o-middleware');
+const {authS3ONoRedirect} = authS3O;
 const proxy = require('express-http-proxy');
 
 const app = express();
+const esEndpoint = process.env.ELASTICSEARCH_URL;
+
+app.use('/_plugin', authS3O, proxy(`${esEndpoint}/_plugin`));
+app.use('/', authS3ONoRedirect, proxy(esEndpoint));
+
+app.listen(3000);
