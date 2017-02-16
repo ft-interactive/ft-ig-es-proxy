97a7f59a182c20c273deba010373a41a68406b3f
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-02-16 10:55:27 +0000

    Changing to express-request-proxy

--- index.js
+++ index.js
@@ -5,12 +5,12 @@
 const express = require('express');
 const authS3O = require('@financial-times/s3o-middleware');
 const {authS3ONoRedirect} = authS3O;
-const proxy = require('express-http-proxy');
+const proxy = require('express-request-proxy');
 
 const app = express();
 const esEndpoint = process.env.ELASTICSEARCH_URL;
 
-app.use('/_plugin', authS3O, proxy(`${esEndpoint}/_plugin`));
-app.use('/', authS3ONoRedirect, proxy(esEndpoint));
+app.use('/_plugin', authS3O, proxy({url: `${esEndpoint}/_plugin`}));
+app.use('/', authS3ONoRedirect, proxy({url: esEndpoint}));
 
 app.listen(process.env.PORT || 8080);
